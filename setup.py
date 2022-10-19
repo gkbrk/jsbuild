a332693c6898b489706aca9dfe737f8d7b04142f
Gokberk Yaltirakli <git@gkbrk.com>
2022-10-19 23:21:24 +0300

    Create setup.py file for PyPI package.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="js-build-cli",
+    py_modules=["jsbuild"],
+    entry_points={"console_scripts": ["jsbuild=jsbuild:main"]},
+    version="0.0.1",
+    description="Low bandwidth DoS tool. Slowloris rewrite in Python.",
+    author="Gokberk Yaltirakli",
+    author_email="opensource@gkbrk.com",
+    url="https://github.com/gkbrk/jsbuild",
+    keywords=["javascript", "build", "package", "minify", "uglify"],
+    license="Apache License 2.0",
+)
