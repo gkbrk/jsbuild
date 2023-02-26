c25e3e1d1b176ef305f59aea747e3e71061aa221
Gokberk Yaltirakli <git@gkbrk.com>
2023-02-26 16:27:57 +0300

    PyPI: Update to v0.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="js-build-cli",
     py_modules=["jsbuild"],
     entry_points={"console_scripts": ["jsbuild=jsbuild:main"]},
-    version="0.0.3",
+    version="0.0.4",
     description="Low bandwidth DoS tool. Slowloris rewrite in Python.",
     author="Gokberk Yaltirakli",
     author_email="opensource@gkbrk.com",
